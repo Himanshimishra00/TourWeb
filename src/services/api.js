60e0dab28f6505849ca8e0daee6e467143050fbe
himanshi <himumishrao2@gmail.com>
2025-06-13 22:24:22 +0530

    final Commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
+console.log(BASE_URL)
 
 export const fetchDestinations = async () => {
   const res = await axios.get(`${BASE_URL}/api/destinations`);
