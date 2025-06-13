c9e90e585ef8d036ef110edaa596d4327a43c289
himanshi <himumishrao2@gmail.com>
2025-06-13 22:08:35 +0530

    final

--- src/services/api.js
+++ src/services/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchDestinations = async () => {
-  const res = await axios.get(`${BASE_URL}/destinations`);
+  const res = await axios.get(`${BASE_URL}/api/destinations`);
   return res.data;
 };
 
 export const fetchTopPackages = async () => {
-  const res = await axios.get(`${BASE_URL}/packages/top-selling`);
+  const res = await axios.get(`${BASE_URL}/api/packages/top-selling`);
   return res.data;
 };
